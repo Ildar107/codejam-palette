8e48a8ea360199ec3d591b85fef6d0e82bf1d207
Ildar107 <ildar107wk@gmail.com>
2019-11-11 21:14:06 +0300

    feat: add alghorithm for pencil

--- palette/scripts/index.js
+++ palette/scripts/index.js
@@ -13,24 +13,22 @@ window.onload = function() {
 
     const blueColor = document.getElementById('blue');
     const redColor = document.getElementById('red');
+    const currColor = document.getElementById('current-color');
+    const prevColor = document.getElementById('prev-color');
+    currColor.style.backgroundColor = color;
+    prevColor.style.backgroundColor  = '#000';
+    let lastX = 0;
+    let lastY = 0;
 
     blueColor.addEventListener('click', () => {
-        color = '#0000ff'
-        if(blueColor.className.search('selected-button') >= 0) {
-            blueColor.className = blueColor.className.replace(/selected-button/g, '')
-        }
-        else {
-            blueColor.className += ' selected-button';
-        }
+        color = '#0000ff';
+        prevColor.style.backgroundColor = currColor.style.backgroundColor;
+        currColor.style.backgroundColor = color;
     });
     redColor.addEventListener('click', () =>{
-        color = '#ff0000'
-        if(redColor.className.search('selected-button') >= 0) {
-            redColor.className = redColor.className.replace(/selected-button/g, '')
-        }
-        else {
-            redColor.className += ' selected-button';
-        }
+        color = '#ff0000';
+        prevColor.style.backgroundColor = currColor.style.backgroundColor;
+        currColor.style.backgroundColor = color;
     });
 
     pencil.addEventListener('click', () => {
@@ -82,43 +80,47 @@ window.onload = function() {
     canvas.addEventListener('mousemove', (e) => {
         if(!isDrawing) { return; }
         if(instrument === 'pencil') { 
-            draw(e);
+            drawWithAlgorithm(lastX, lastY, e.offsetX, e.offsetY);
+            lastX = e.offsetX;
+            lastY = e.offsetY;
         }
     });
     canvas.addEventListener('mousedown', (e) => { 
         isDrawing = true; 
+        lastX = e.offsetX;
+        lastY = e.offsetY;
         if(instrument === 'bucket') { 
             fillCanvasZone(e);
         }
         if(instrument === 'pencil') { 
-            draw(e);
+            draw(new Point(e.offsetX, e.offsetY));
+            lastX = e.offsetX;
+            lastY = e.offsetY;
         }
     });
     canvas.addEventListener('mouseup', (e) => isDrawing = false);
-  };
+};
 
-  function draw(e) {
-    console.log(`x = ${e.offsetX}   y = ${e.offsetY}, startX = ${Math.ceil(e.offsetX/32)*32}  startY = ${Math.ceil(e.offsety/32)*32}`)
+function draw(point) {
     const canvas = document.getElementById('work-canvas');
     const ctx = canvas.getContext("2d");
-    const point = getRectStartPoint(new Point(e.offsetX, e.offsetY));
+    point = getRectStartPoint(point);
     ctx.fillStyle = color;
     ctx.fillRect(point.x, point.y, matrixSize, matrixSize );
-  }
+}
 
-    function getRectStartPoint(point) {
-        return new Point(Math.floor(point.x/matrixSize)*matrixSize, Math.floor(point.y/matrixSize)*matrixSize);
-    }
+function getRectStartPoint(point) {
+    return new Point(Math.floor(point.x/matrixSize)*matrixSize, Math.floor(point.y/matrixSize)*matrixSize);
+}
 
-  function fillCanvasZone(e){
+function fillCanvasZone(e){
     const canvas = document.getElementById('work-canvas');
     const ctx = canvas.getContext("2d");
-    let startPoint = getRectStartPoint(new Point(e.offsetX, e.offsetY));
+    const startPoint = getRectStartPoint(new Point(e.offsetX, e.offsetY));
     ctx.fillStyle = color;
-    const sumColor = ctx.getImageData(startPoint.x, startPoint.y, 1, 1).data.reduce((x,y) => x + y, 0);
-    let queue = [startPoint];
-    let usedPoint =  new Set();
-    let count = 0;
+    const sumColor = ctx.getImageData(startPoint.x, startPoint.y, 1, 1).data.reduce((x,y,i) => x + y*(i+1), 0);
+    const queue = [startPoint];
+    const usedPoint =  new Set();
     while(queue.length > 0) {
         let point = queue.shift();
         if(usedPoint.has(point.toString())) { 
@@ -128,7 +130,7 @@ window.onload = function() {
         if(point.x < 0 || point.x > ctx.width || point.y < 0 || point.y > ctx.width) {
             continue;
         }
-        let newSumColor = ctx.getImageData(point.x, point.y, 1, 1).data.reduce((x,y) => x + y, 0);
+        let newSumColor = ctx.getImageData(point.x, point.y, 1, 1).data.reduce((x,y,i) => x + y*(i+1), 0);
         if(newSumColor !== sumColor) {
             continue;
         }
@@ -140,24 +142,47 @@ window.onload = function() {
     }
   }
 
-  function setPictureToCanvas()
-  {
-    const canvas = document.getElementById('work-canvas')
-    const ctx = canvas.getContext("2d");
-    const image = new Image();
-    image.src = './images/start-image.png';
-    ctx.width = 512;
-    ctx.height = 512;
-    image.onload = () => ctx.drawImage(image, 0, 0,512,512);
-  } 
-
-  class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+function drawWithAlgorithm(x1, y1, x2, y2) {
+    const point1 = getRectStartPoint(new Point(x1, y1));
+    const point2 = getRectStartPoint(new Point(x2, y2));
+    const deltaX = Math.abs(point2.x - point1.x);
+    const deltaY = Math.abs(point2.y - point1.y);
+    const signX = point1.x < point2.x ? matrixSize : -matrixSize;
+    const signY = point1.y < point2.y ? matrixSize : -matrixSize;
+    let error = deltaX - deltaY;
+    draw(point2);
+    while(point1.x != point2.x || point1.y != point2.y) {
+        draw(point1);
+        let error2 = error * 2;
+        if(error2 > -deltaY) {
+            error -= deltaY;
+            point1.x += signX;
+        }
+        if(error2 < deltaX) {
+            error += deltaX;
+            point1.y += signY;
+        }
     }
+}
 
-    toString(){
-        return `${this.x} ${this.y}`;
-    }
-  }
+function setPictureToCanvas()
+{
+const canvas = document.getElementById('work-canvas')
+const ctx = canvas.getContext("2d");
+const image = new Image();
+image.src = './images/start-image.png';
+ctx.width = 512;
+ctx.height = 512;
+image.onload = () => ctx.drawImage(image, 0, 0,512,512);
+} 
+
+class Point {
+constructor(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+toString(){
+    return `${this.x} ${this.y}`;
+}
+}
